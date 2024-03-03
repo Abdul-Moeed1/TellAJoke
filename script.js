fb7d92345542e62984a924b8bf5c322d508ea119
Abdul-Moeed1 <91484014+Abdul-Moeed1@users.noreply.github.com>
2024-03-03 18:05:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,320 @@
+let jokes = [` Did you hear about the first restaurant to open on the moon?It had great food, but no atmosphere.`,
+
+` Why don’t scientists trust atoms?
+Because they make up everything!`,
+
+` What do dentists call their x-rays?
+Tooth pics!`,
+
+` What did one ocean say to the other ocean?
+Nothing, it just waved.`,
+
+` Do you want to hear a construction joke?
+Sorry, I’m still working on it.`,
+
+
+`
+ Did you hear about the fire at the circus?
+It was in tents!`,
+
+` Why do ducks have feathers?
+To cover their butt quacks!`,
+
+` What’s the difference between a hippo and a zippo?
+One is really heavy and the other’s a little lighter.`,
+
+` What does a nosey pepper do?
+It gets jalapeño business.`,
+
+` Why should you never trust stairs?
+They’re always up to something.`,
+
+` When does a joke become a ‘dad’ joke?
+When it becomes apparent.`,
+
+` Why did the bullet end up losing his job?
+He got fired.`,
+
+` What kind of shorts do clouds wear?
+Thunderpants`,
+
+` I entered ten puns in a contest to see which would win.
+No pun in ten did.`,
+
+` How do you measure a snake?
+In inches—they don’t have feet.`,
+
+` Where does a waitress with only one leg work?
+IHOP.`,
+
+` What does a house wear?
+Address!`,
+
+` Why are toilets always so good at poker?
+They always get a flush`,
+
+` Why is Peter Pan always flying?
+Because he Neverlands. (I love this joke because it never grows old.)`,
+
+` You heard the rumor going around about butter?
+Never mind, I shouldn’t spread it.`,
+
+` Two windmills are standing on a wind farm. One asks, ‘What’s your favorite kind of music?’
+The other replies, ‘I’m a big metal fan.’`,
+
+` The first rule of the Alzheimer’s club is…
+Wait, where are we again?`,
+
+` I took the shell off of my racing snail, thinking it would make him faster.
+But if anything, it made him more sluggish.`,
+
+` What do you get from a pampered cow?
+Spoiled milk.`,
+
+` How does NASA organize a party?
+They planet.`,
+
+` What’s the best thing about Switzerland?
+I don’t know, but the flag is a big plus.`,
+
+` Why did the bicycle fall over?
+Because it was two-tired!`,
+
+` How many tickles does it take to get an octopus to laugh?
+Ten tickles`,
+
+` Why doesn’t Dracula have any friends?
+Well, honestly, he’s a real pain in the neck.`,
+
+` My teachers told me I’d never amount to much since I procrastinate so much.
+I told them, “Just you wait!”`,
+
+` Why were they called the “dark ages?”
+Because there were a lot of knights.`,
+
+` What gets wetter the more it dries?
+A towel.`,
+
+` Why aren’t koalas considered bears?
+They don’t have the right koala-fications.`,
+
+` You: What cartoon mouse walks on two feet?
+
+Them: Mickey Mouse
+
+You: What duck walks on two feet?
+
+Them: Donald Duck
+
+You: No, all ducks do!
+`,
+` Want to hear a joke about a roof?
+The first one’s on the house.`,
+
+` What’s a pirate’s favorite letter?
+You probably think it’s “R” but it be the “C”.`,
+
+` How much teddy bears never want to eat anything?
+Because they’re always stuffed.`,
+
+` Did you hear about the cheese factory that exploded in France?
+There was nothing left but de Brie.`,
+
+` Where should you go in the room if you’re feeling cold?
+The corner—they’re usually 90 degrees.`,
+
+` I can never take my dog to the park because the ducks keep trying to bite him.
+I guess that’s what I get for buying a pure bread dog.`,
+
+` What’s the difference between a poorly dressed man on a unicycle and a well-dressed man on a bicycle?
+Attire.`,
+
+` What did the Buddhist ask the hot dog vendor?
+“Make me one with everything.”`,
+
+` You know why you never see elephants hiding up in trees?
+Because they’re really good at it.`,
+
+` A horse walks into a bar.
+The bartender says, “Why the long face?”`,
+
+` How did the hipster burn his mouth?
+He ate his pizza before it was cool.`,
+
+
+` What do you get when you cross a dyslexic, an insomniac, and an agnostic?
+Someone who lays awake at night wondering if there’s a dog.`,
+
+` As a scarecrow, people say I’m outstanding in my field.
+But hay, it’s in my jeans.`,
+
+` Why don’t blind people skydive?
+Because it scares their dogs.`,
+
+` What’s the difference between an oral thermometer and a rectal thermometer?
+The taste, mostly.`,
+
+` I stayed up all night and tried to figure out where the sun was.
+Then it dawned on me.`,
+             
+` I told my friend ten jokes to make him laugh.
+Sadly, no pun in ten did.`,
+
+` I couldn’t believe the highway department called my dad a thief.
+But when I got home, the signs were all there.`,
+
+` Why did the cowboy get a wiener dog?
+He wanted to get a long little doggie.`,
+
+` Can a kangaroo jump higher than the Empire State Building?
+Of course! The Empire State Building can’t jump.`,
+
+` What did the duck say when it bought some lipstick?
+“Put it on my bill.”`,
+
+` George Clooney, Leonardo DiCaprio, and Matthew McConaughey get together to make a movie.
+
+Clooney says, “I’ll direct.”
+
+DiCaprio says, “I’ll act.”
+
+McConaughey says, “I’ll write, I’ll write, I’ll write.”
+`,
+             
+` A horse walks into a bar.
+
+The bartender says, “Hey!”
+
+The horse replies, “Sure.”
+`,
+             
+` I googled “Rorshach test.”
+But for some reason, all that came up were pictures of my parents fighting.`,
+
+` What do we want?
+
+Low-flying airplane noises!
+
+When do we want them?
+
+Nnnnneeeeeeeeeeoooooooooow!
+`,
+` I tried to win a suntanning competition.
+But all I got was bronze.`,
+
+` What do you call fake spaghetti?
+An impasta!`,
+
+` Why don’t seagulls fly over the bay?
+Because then they’d be bagels.`,
+
+` What do you call malware on a Kindle?
+A bookworm.`,
+
+` Did you hear about the painter who was hospitalized?
+The doctors say it was due to too many strokes.`,
+
+` Why are crabs so bad at sharing?
+Because they’re all shellfish.`,
+
+
+` What did the tie say to the hat?
+You go on ahead. I’ll hang around.`,
+
+` How do you make a tissue dance?
+Put a little boogie in it.`,
+
+` What do you call a cow with a twitch?
+Beef jerky.`,
+
+` I started a new job as a tailor last week.
+It’s been sew-sew.`,
+
+` What concert only costs 45 cents?
+50 Cent featuring Nickleback.`,
+
+` What kind of shoes does a spy wear?
+Sneakers.`,
+
+` I’ve been trying to make a sarcastic club, but it’s been really hard to tell if people are interested in joining or not.`,
+
+` And the Lord said unto John, “Come forth and you will receive eternal life.”
+But John came fifth and won a toaster.`,
+
+` Why don’t some fish play piano?
+Because you can’t tuna fish!`,
+
+` My wife accused me the other day of being too immature.
+I told her there were no girls allowed in my fort.`, `Why don't scientists trust atoms? Because they make up everything.`,
+              `Parallel lines have so much in common. It’s a shame they’ll never meet.`,
+              `I told my wife she was drawing her eyebrows too high. She looked surprised.`,
+              `I only know 25 letters of the alphabet. I don't know y.`,
+              `Why don't skeletons fight each other? They don't have the guts.`,
+              `I used to play piano by ear, but now I use my hands and fingers.`,
+              `What do you call a fish wearing a crown? A kingfish.`,
+              `Why don't scientists trust atoms? Because they make up everything.`,
+              `I'm on a whiskey diet. I've lost three days already.`,
+              `Why did the scarecrow win an award? Because he was outstanding in his field.`,
+              `I told my computer I needed a break, and now it won't stop sending me vacation ads.`,
+              `Why did the math book look sad? Because it had too many problems.`,
+              `Why do programmers prefer dark mode? Because light attracts bugs.`,
+              `I told my wife she was drawing her eyebrows too high. She looked surprised.`,
+              `What do you call a factory that makes good products? A satisfactory.`,
+              `I used to be a baker because I kneaded dough.`,
+              `Why don't scientists trust atoms? Because they make up everything.`,
+              `Why did the bicycle fall over? Because it was two-tired.`,
+              `I'm reading a book on anti-gravity. It's impossible to put down!`,
+              `Parallel lines have so much in common. It’s a shame they’ll never meet.`,
+              `I'm reading a book on anti-gravity. It's impossible to put down!`,
+              `I told my wife she was drawing her eyebrows too high. She looked surprised.`,
+              `What do you get when you cross a snowman and a vampire? Frostbite.`,
+              `I only know 25 letters of the alphabet. I don't know y.`,
+              `What did the ocean say to the shore? Nothing, it just waved.`,
+              `I used to be a baker because I kneaded dough.`,
+              `Why don't skeletons fight each other? They don't have the guts.`,
+              `Why did the scarecrow win an award? Because he was outstanding in his field.`,
+              `What did one hat say to the other? Stay here, I'm going on ahead.`,`Why did Mickey Mouse become an astronaut? Because he wanted to visit Pluto!`,
+              `What do you call a fish who wears a crown? Nemo-king!`,
+              `Why did the cartoon character go to therapy? To deal with his sketchy past!`,
+              `What's Bugs Bunny's favorite type of music? Hip-hop, of course!`,
+              `Why did the scarecrow become a successful cartoonist? He was outstanding in his field!`,
+              `How does SpongeBob SquarePants clean his home? With his undersea-broom!`,
+              `What do you call a bear with no teeth watching cartoons? A gummy bear!`,
+              `Why did the cartoon cat go to school? To improve his mouse-trapping skills!`,
+              `What's Donald Duck's favorite dance? The quackstep!`,
+              `Why was the cartoon broom so confident? It swept the competition away!`,
+              `What did one animated pencil say to another? "You draw me crazy!"`,
+              `How do you fix a broken tuba in a cartoon? With tuba glue!`,
+              `Why did the cartoon tomato turn red? Because it saw the salad dressing!`,
+              `What's a cartoon character's favorite breakfast? Toaster strudel, of course!`,
+              `Why did the cartoon character go to therapy? To work on his "toon" issues!`,
+              `What do you call a fish who wears a crown? Nemo-archy!`,
+              `How does a cartoonist cope with mistakes? They draw and quarter them!`,
+              `What did the animated bee say to the flower? "Hi, bud!"`,
+              `Why did the cartoon chicken join a band? It had the drumsticks!`,
+              `What's the Flintstones' favorite game? Rock, paper, scissors!`,
+              `Why did the cartoon cow go to space? To see the moooon!`,
+              `What's a cartoon character's favorite dessert? Jellybean pie!`,
+              `Why did the cartoon car feel blue? It had the transmission blues!`,
+              `What do you call a cartoon character who sings? A toon-talist!`,
+              `How do you catch a squirrel in a cartoon? Climb a tree and act like a nut!`,
+              `What's Scooby-Doo's favorite kind of pizza? Mystery meat!`,
+              `Why did the cartoon pencil go to school? To learn how to draw a line!`,
+              `What did the cartoon cat use to fix its house? A mouse pad!`,
+              `Why was the cartoon skunk so good at math? It had a natural sense of "odor" magnitude!`]
+let num = Math.floor(Math.random() * jokes.length);
+let joke = jokes[num];
+document.getElementById("joke").innerHTML=joke;
+function newj()
+{
+  num = Math.floor(Math.random() * jokes.length);
+  joke = jokes[num];
+  document.getElementById("joke").innerHTML=joke;
+  console.log(`num:${num}`)
+}
+function clr(){
+  document.getElementById("btn").style.background="aqua";
+}
+function c(){
+  document.getElementById("btn").style.background="#00ff00";
+}
